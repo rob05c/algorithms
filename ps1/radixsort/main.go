a1319b30396d7354bf1e5ae7b32e0c756eb5cfd1
Robert Butts <robert.o.butts@gmail.com>
2015-01-28 19:27:46 -0700

    added radixsort, data files

---
+++ ps1/radixsort/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var inputfile = flag.String("i", "", "The data input file to read from")
+
+func getdata(filename string) ([]int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		file.Close() /// \todo handle error
+	}()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // scan the header line
+	scanner.Scan() // scan the length of the file
+	len, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]int, len, len)
+
+	for i := 0; scanner.Scan(); i++ {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		data[i] = val
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+/// This is a subset of a true radix sort, for input values from [0..99].
+/// A true radix sort for arbitrary integers is significantly slower, having to check many more digits.
+func restrictedRadixSort(data []int) []int {
+	buckets := make([]int, 100, 100)
+
+	for _, val := range data {
+		buckets[val]++
+	}
+
+	pos := 0
+	for value, numberOfElements := range buckets {
+		for i := 0; i != numberOfElements; i++ {
+			data[pos] = value
+			pos++
+		}
+	}
+
+	return data
+}
+
+func main() {
+	flag.Parse()
+
+	if *inputfile == "" {
+		fmt.Println("Usage:")
+		flag.PrintDefaults()
+		return
+	}
+
+	data, err := getdata(*inputfile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(data)
+
+	fmt.Println("sorting...")
+	data = restrictedRadixSort(data)
+
+	fmt.Println(data)
+}
